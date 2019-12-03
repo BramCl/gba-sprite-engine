dd0bf4bc025e5aac72ed00a07e03fc233d79c94c
BramCl <claessen.bram@gmail.com>
2019-12-03 20:07:16 +0100

    bewegen en diepte
    Het is nu mogelijk om te 'bewegen' en links boven staat de diepte

--- demos/demo1-basicfeatures/src/flying_stuff_scene.cpp
+++ demos/demo1-basicfeatures/src/flying_stuff_scene.cpp
@@ -6,12 +6,13 @@
 #include <libgba-sprite-engine/sprites/sprite_builder.h>
 #include <libgba-sprite-engine/gba/tonc_memmap.h>
 #include <libgba-sprite-engine/background/text_stream.h>
+#include <libgba-sprite-engine/gba/tonc_memdef.h>
 #include "flying_stuff_scene.h"
 #include "kul.h"
 
 std::vector<Sprite *> FlyingStuffScene::sprites() {
     return {
-        smiley.get(), kul.get(), kulFlying.get(), player.get()
+        player.get()
     };
 }
 
@@ -28,42 +29,30 @@ void FlyingStuffScene::load() {
     SpriteBuilder<Sprite> builder;
     SpriteBuilder<AffineSprite> affineBuilder;
 
-    smiley = builder
-            .withData(piskelTiles, sizeof(piskelTiles))
-            .withSize(SIZE_16_16)
-            .withLocation(10, 10)
-            .buildPtr();
-
-    kul = builder
-            .withData(kulTiles, sizeof(kulTiles))
-            .withSize(SIZE_64_32)
-            .withLocation(30, 30)
-            .buildPtr();
-
-    kulFlying = affineBuilder
-            .withData(kulTiles, sizeof(kulTiles))
-            .withSize(SIZE_64_32)
-            .withLocation(100, 50)
-            .withVelocity(0, 0)
-            .buildPtr();
-
     player = affineBuilder
             .withData(piskel2Tiles, sizeof(piskel2Tiles))
             .withSize(SIZE_16_16)
-            .withLocation(150, 60)
+            .withLocation(120, 80)
             .buildPtr();
-
-    TextStream::instance() << "level 1 ofwa" << "ziet da af";
-
     bg = std::unique_ptr<Background>(new Background(1, background_data, sizeof(background_data), map, sizeof(map)));
     bg.get()->useMapScreenBlock(16);
 }
 
 void FlyingStuffScene::tick(u16 keys) {
-    scrollX += 1;
 
-    rotation += rotationDiff;
-    kulFlying.get()->rotate(rotation);
-    player.get()->rotate(rotation);
+    TextStream::instance().setText(std::to_string(-scrollY), 0, 0);
+
     bg.get()->scroll(scrollX, scrollY);
+
+    if(keys & KEY_LEFT) {
+        scrollX -= 2;
+    } else if(keys & KEY_RIGHT) {
+        scrollX += 2;
+    } else if(keys & KEY_UP) {
+        scrollY -= 2;
+    } else if(keys & KEY_DOWN) {
+        scrollY += 2;
+    } else {
+        player->setVelocity(0, 0);
+    }
 }
--- demos/demo1-basicfeatures/src/flying_stuff_scene.h
+++ demos/demo1-basicfeatures/src/flying_stuff_scene.h
@@ -9,20 +9,17 @@
 #include <libgba-sprite-engine/sprites/affine_sprite.h>
 #include <libgba-sprite-engine/scene.h>
 #include <libgba-sprite-engine/background/background.h>
+#include <libgba-sprite-engine/background/text_stream.h>
 
 class FlyingStuffScene : public Scene {
 private:
-    std::unique_ptr<Sprite> smiley;
+
     std::unique_ptr<AffineSprite> player;
-    std::unique_ptr<Sprite> kul;
-    std::unique_ptr<AffineSprite> kulFlying;
     std::unique_ptr<Background> bg;
 
-    int scrollX, scrollY;
-    int rotation;
-    int rotationDiff = 128;
+    int scrollX, scrollY, rotateA;
 public:
-    FlyingStuffScene(std::shared_ptr<GBAEngine> engine) : Scene(engine), rotation(0), rotationDiff(128), scrollX(0), scrollY(0) {}
+    FlyingStuffScene(std::shared_ptr<GBAEngine> engine) : Scene(engine), scrollX(0), scrollY(0), rotateA(0) {}
 
     std::vector<Sprite *> sprites() override;
     std::vector<Background *> backgrounds() override;
