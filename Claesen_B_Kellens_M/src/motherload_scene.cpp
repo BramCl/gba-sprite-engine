8f044fcaa9c733c9b82fda926ebe779439e05d5c
maikelkellens <maikel.kellens@gmail.com>
2019-12-08 20:55:29 +0100

    nieuw project - tussentijds
    deels het nieuw project aangemaakt, moet nu foto's en dergelijke goed zetten

---
+++ Claesen_B_Kellens_M/src/motherload_scene.cpp
@@ -0,0 +1,62 @@
+//
+// Created by maike on 8/12/2019.
+//
+
+//
+// Created by Wouter Groeneveld on 28/07/18.
+//
+
+#include <libgba-sprite-engine/sprites/affine_sprite.h>
+#include <libgba-sprite-engine/sprites/sprite_builder.h>
+#include <libgba-sprite-engine/gba/tonc_memmap.h>
+#include <libgba-sprite-engine/background/text_stream.h>
+#include <libgba-sprite-engine/gba/tonc_memdef.h>
+#include "motherload_scene.h"
+// HIER het mannetje #include "kul.h"
+
+std::vector<Sprite *> MotherloadScene::sprites() {
+    return {
+            player.get()
+    };
+}
+
+std::vector<Background *> MotherloadScene::backgrounds() {
+    return {
+            bg.get()
+    };
+}
+// HIER HET ROOD DE KARAKTERS NOG
+void MotherloadScene::load() {
+    foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(new ForegroundPaletteManager(character_onbewerkt_transparant_16Pal, sizeof(character_onbewerkt_transparant_16Pal)));
+    backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager(bg_palette, sizeof(bg_palette)));
+
+    SpriteBuilder<Sprite> builder;
+    SpriteBuilder<AffineSprite> affineBuilder;
+
+    player = affineBuilder
+            .withData(character_onbewerkt_transparant_16Tiles, sizeof(character_onbewerkt_transparant_16Tiles))
+            .withSize(SIZE_32_32)
+            .withLocation(120, 80)
+            .buildPtr();
+    bg = std::unique_ptr<Background>(new Background(1, background_data, sizeof(background_data), map, sizeof(map)));
+    bg.get()->useMapScreenBlock(16);
+}
+
+void MotherloadScene::tick(u16 keys) {
+
+    TextStream::instance().setText(std::to_string(-scrollY), 0, 0);
+
+    bg.get()->scroll(scrollX, scrollY);
+
+    if(keys & KEY_LEFT) {
+        scrollX -= 2;
+    } else if(keys & KEY_RIGHT) {
+        scrollX += 2;
+    } else if(keys & KEY_UP) {
+        scrollY -= 2;
+    } else if(keys & KEY_DOWN) {
+        scrollY += 2;
+    } else {
+        scrollY += 1;
+    }
+}
---
+++ Claesen_B_Kellens_M/src/motherload_scene.h
@@ -0,0 +1,32 @@
+//
+// Created by maike on 8/12/2019.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_MOTHERLOAD_SCENE_H
+#define GBA_SPRITE_ENGINE_PROJECT_MOTHERLOAD_SCENE_H
+
+
+#include <libgba-sprite-engine/sprites/sprite.h>
+#include <libgba-sprite-engine/sprites/affine_sprite.h>
+#include <libgba-sprite-engine/scene.h>
+#include <libgba-sprite-engine/background/background.h>
+#include <libgba-sprite-engine/background/text_stream.h>
+
+class MotherloadScene : public Scene {
+private:
+
+    std::unique_ptr<AffineSprite> player;
+    std::unique_ptr<Background> bg;
+
+    int scrollX, scrollY, rotateA;
+public:
+    MotherloadScene(std::shared_ptr<GBAEngine> engine) : Scene(engine), scrollX(0), scrollY(0), rotateA(0) {}
+
+    std::vector<Sprite *> sprites() override;
+    std::vector<Background *> backgrounds() override;
+
+    void load() override;
+    void tick(u16 keys) override;
+};
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_MOTHERLOAD_SCENE_H
